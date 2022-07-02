a2179c6a5b8d357e5812d2c1b1983b11d46700dc
Arthur McLain <mclain.it@gmail.com>
2022-07-02 11:03:51 +0200

    Create an api and a view for it

--- music_controller/api/urls.py
+++ music_controller/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import main
+from .views import RoomView, CreateRoom
 
 urlpatterns = [
-    path('', main)
+    path('room/', RoomView.as_view()),
+    path('new/', CreateRoom.as_view()),
 ]
--- music_controller/api/views.py
+++ music_controller/api/views.py
@@ -1,7 +1,15 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Room
+from .serializers import RoomSerializer
 
 
 # Create your views here.
-def main(request):
-    return HttpResponse("<h1>It Works</h1>")
+class RoomView(generics.ListAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
+
+
+class CreateRoom(generics.CreateAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
